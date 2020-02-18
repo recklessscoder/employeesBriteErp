f13ab0654a71077b6e329fe421101638fc7789af
aybukego <aybukeegorgulu@gmail.com>
2020-02-18 09:48:52 -0600

    Work in progress 3 of the test cases

--- src/test/java/pages/EmployeeTabPage.java
+++ src/test/java/pages/EmployeeTabPage.java
@@ -1,9 +1,13 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.Driver;
+
+import java.util.List;
 
 public class EmployeeTabPage {
 
@@ -23,6 +27,21 @@ public class EmployeeTabPage {
     @FindBy (xpath = "//div[@class = 'oe_kanban_global_click o_kanban_record'][1]")
     public WebElement employeeProfileKanban;
 
+    @FindBy(xpath = "//button[@class=\"o_follow_btn o_follow btn btn-sm btn-primary oe_kanban_action oe_kanban_action_button\"]" )
+    public List <WebElement> followButton;
 
-    // (//span[@class = 'oe_menu_text'])[17]
+
+    @FindBy(xpath = "/html/body/div[1]/div[1]/div[1]/div[7]/ul/li/a/span")
+    public  WebElement badges;
+
+    @FindBy(xpath = "//button[@class=\"btn btn-icon fa fa-lg fa-th-large o_cp_switch_kanban active\"]")
+    public WebElement gridButton;
+
+    @FindBy(xpath = "//button[@class=\"btn btn-icon fa fa-lg fa-list-ul o_cp_switch_list\"]")
+    public WebElement listButton;
+
+
+
+
+
 }
