d39a8d1d11dfb2eb57d4b214c96a6e93e4b8af2f
AntonSemenov <Anthony04061987@gmail.com>
2020-02-20 11:15:38 -0600

    added file 204

--- src/test/java/testExecution/PROQ_204.java
+++ src/test/java/testExecution/PROQ_204.java
@@ -1,5 +1,4 @@
 package testExecution;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -9,25 +8,19 @@ import pages.EmployeeTabPage;
 import pages.TimesheetPage;
 import utilities.Driver;
 import utilities.Reusable;
-
 public class PROQ_204 {
-
     @Test
     public void tc204(){
         Reusable.login_nav_to_employee_tab();
-
         EmployeeTabPage employeeTabPage = new EmployeeTabPage();
         EmployeeDetailPage employeeDetailPage = new EmployeeDetailPage();
         TimesheetPage timesheetPage = new TimesheetPage();
         // click first employee tab
         employeeTabPage.employeeProfileKanban.click();
-
         // click timesheet button
         employeeDetailPage.timesheetButton.click();
-
         //click on create button
         timesheetPage.createButton.click();
-
         //type 1 of the employee
         timesheetPage.employeeDropDownButton.sendKeys("Abuzer Kadayif");
         // fill up any message on description field
@@ -38,17 +31,6 @@ public class PROQ_204 {
         WebElement emptyField = Driver.getDriver().findElement(By.xpath("(//input[@class = 'o_input ui-autocomplete-input'])[3]") );
         //get the text
         String verify = emptyField.getText();
-
         Assert.assertTrue(verify.isEmpty());
-
-        // Nav to Empl tab
-        Reusable.return_to_employee_page();
-
-
-
-
-
-
-
     }
 }
